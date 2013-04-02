72755b6baea4c94c1b40ef89addf49bab6e12bda
ralston <ralston@loku.com>
2013-04-02 11:46:08 -0700

    board directive locally and on goko

---
+++ app.js
@@ -0,0 +1,28 @@
+
+angular.element(document).ready(function(){
+  var $app = $('<div id="isogoko-app" ng-app="isogoko"></div>');
+  $app.append('<board></board>');
+  
+  $('body').append($app);
+
+  angular.bootstrap($app[0], ['isogoko']);
+});
+
+angular.module('isogoko', [], function(){
+})
+
+.directive('board', [function(){
+  
+  var dir = {
+    templateUrl: window.chrome && window.chrome.extension && window.chrome.extension.getURL('board.html') || 'board.html',
+    replace:true,
+    restrict:'E'
+  };
+
+  dir.link = function boardLink(scope, element, attrs){
+    console.log('linking isogoko directive');
+  };
+
+  return dir;
+
+}]);
